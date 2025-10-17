613ddc1807c550b8dba09020dcac92254dbd9c39
Sveta <svetamashenko@yandex.ru>
2025-10-17 13:16:50 +0300

    Homework 1.

---
+++ Assets/PixelCrew/Hero.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    [SerializeField] private int _speed;
+
+    private Vector2 _direction;
+    public void SetDirection(Vector2 direction)
+    {
+        _direction = direction;
+    }
+
+    private void Update()
+    {
+        if (_direction != new Vector2(0, 0))
+        {
+            var delta = _speed * Time.deltaTime * _direction;
+            var new2DPos = new Vector2(transform.position.x, transform.position.y) + delta;
+            transform.position = new Vector3(new2DPos.x, new2DPos.y, transform.position.z);
+        }
+    }
+
+    public void SaySomething()
+    {
+        Debug.Log("Something...");
+    }
+}
---
+++ Assets/PixelCrew/HeroInputReader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HeroInputReader : MonoBehaviour
+{
+    [SerializeField] private Hero _hero;
+
+    private void OnMovement(InputValue context)
+    {
+        var direction = context.Get<Vector2>();
+        _hero.SetDirection(direction);
+    }
+
+    private void OnSaySomething(InputValue context)
+    {
+        _hero.SaySomething();
+    }
+}
