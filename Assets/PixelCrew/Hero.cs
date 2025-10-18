2866de01f533f988c027f6abeb73501f0badb2e4
Sveta <svetamashenko@yandex.ru>
2025-10-18 15:09:27 +0300

    Homework 2.

--- Assets/PixelCrew/Hero.cs
+++ Assets/PixelCrew/Hero.cs
@@ -3,21 +3,43 @@
 public class Hero : MonoBehaviour
 {
     [SerializeField] private int _speed;
+    [SerializeField] private int _jumpSpeed;
+    [SerializeField] private LayerCheck _groundCheck;
 
+    private Rigidbody2D _rigidbody;
     private Vector2 _direction;
     public void SetDirection(Vector2 direction)
     {
         _direction = direction;
     }
 
-    private void Update()
+    private void Awake()
     {
-        if (_direction != new Vector2(0, 0))
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
+
+        var isJumping = _direction.y > 0;
+
+        if (isJumping)
         {
-            var delta = _speed * Time.deltaTime * _direction;
-            var new2DPos = new Vector2(transform.position.x, transform.position.y) + delta;
-            transform.position = new Vector3(new2DPos.x, new2DPos.y, transform.position.z);
+            if (IsGrounded())
+            {
+                _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+            }
         }
+        else if (_rigidbody.velocity.y > 0)
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return _groundCheck.IsTouchingLayer;
     }
 
     public void SaySomething()
