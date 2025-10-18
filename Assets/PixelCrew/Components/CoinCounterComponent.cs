ee87cde274a8216d3de092f18c58ad2c4e928b9c
Sveta <svetamashenko@yandex.ru>
2025-10-18 15:09:27 +0300

    Exercise CoinsCounter.

---
+++ Assets/PixelCrew/Components/CoinCounterComponent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCounterComponent : MonoBehaviour
+{
+    [SerializeField] private int _coinBalance;
+
+    public void AddToBalance(int coinValue)
+    {
+        _coinBalance += coinValue;
+        ShowBalance();
+    }
+
+    public void ShowBalance()
+    {
+        Debug.Log($"Player has {_coinBalance} currency.");
+    }
+}
